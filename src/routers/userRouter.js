556daecdca5f5bab85ddca5ffd803f6828e4b1f6
iyasn <moreira.aquino@aluno.ifsp.edu.br>
2023-09-04 17:51:08 -0300

    a

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import getUser from '../controllers/user/getUser.js'
+import listUsers from '../controllers/user/listUsers.js'
 import insertUser from '../controllers/user/insertUser.js'
 import updateUser from '../controllers/user/updateUser.js'
 import deleteUser from '../controllers/user/deleteUser.js'
 
-
 const router = express.Router()
 
 router.get('/', getUser)
+router.get('/list', listUsers)
 router.post('/', insertUser)
-router.delete('/', deleteUser)
 router.put('/', updateUser)
+router.delete('/', deleteUser)
 
 export default router
