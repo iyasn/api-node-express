a44d4aec7d76cc965b0d4178fe59263fd8a609ec
iyasn <moreira.aquino@aluno.ifsp.edu.br>
2023-10-02 17:37:53 -0300

    a

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const PORT = 3000
+export const PORT = 3300
 
 export const DB_HOST = 'localhost'
 export const DB_USER = 'root'
 export const DB_PASS = ''
-export const DB_NAME = 'api-node'
+export const DB_NAME = 'api-web'
